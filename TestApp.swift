7d45e111093f74358234abd45b9e0ae9f3d22e54
Elvis Stansvik <elvis.stansvik@orexplore.com>
2021-08-18 22:00:50 +0200

    Set CORESVG_VERBOSE in the environment

--- TestApp.swift
+++ TestApp.swift
@@ -5,7 +5,9 @@ import Darwin
 struct TestApp: App {
     init() {
         setlocale(LC_NUMERIC, "sv_SE.UTF-8")
-        let timer = Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { timer in
+        setenv("CORESVG_VERBOSE", "1", 1)
+
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { timer in
             exit(0)
         }
     }
