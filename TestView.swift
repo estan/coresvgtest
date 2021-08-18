dd968afaeff631d9476881c3da99b7bc6bcb0279
Elvis Stansvik <elvis.stansvik@orexplore.com>
2021-08-18 19:44:03 +0200

    See if NSImage likes this better

--- TestView.swift
+++ TestView.swift
@@ -1,8 +1,14 @@
 import AppKit
 import SwiftUI
 
+let svg = """<?xml version="1.0" encoding="utf-8"?>
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+    <path fill="white" d="M 4 2 L 6.195312 2 L 6.195312 8.996094 L 4 8.996094 Z"/>
+</svg>
+""".data(using: .utf8)!
+
 struct TestView: View {
     var body: some View {
-        Image(nsImage: NSImage(data: "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 10 10\"><path fill=\"white\" d=\"M 4 2 L 6.195312 2 L 6.195312 8.996094 L 4 8.996094 Z\"/></svg>".data(using: .utf8)!)!)
+        Image(nsImage: NSImage(data: svg)!)
     }
 }
